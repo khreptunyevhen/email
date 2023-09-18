e931aaea195cb1f209d43fa68a49900c855efcb5
Yevhen Khreptun <khreptun.e@gmail.com>
2023-09-18 15:14:50 -0400

    Add recipients email

--- js/script.js
+++ js/script.js
@@ -24,11 +24,17 @@ const users = [
     name: "Peter",
     email: "peter@gmail.com",
   },
+  {
+    id: 5,
+    name: "Pet",
+    email: "pet@gmail.com",
+  },
 ];
 
 const emailBodyEl = document.querySelector(".email-body");
 const emailEl = document.querySelector(".email");
 const userListEl = document.querySelector("#users-list");
+const recipientsBodyEl = document.querySelector("#recipients");
 
 const collapseEl = document.querySelector(".collapse");
 const expandEl = document.querySelector(".expand");
@@ -39,13 +45,25 @@ const emailInput = document.querySelector("#to-user");
 // Show/hide an email
 function collapseElement(el) {
   el.classList.toggle("hide");
-
-  //   el.style.height = "0px";
 }
 
 // Expand/reduce an email
 function expandElement(el) {
   el.classList.toggle("expand");
+}
+
+// Add recipient emails
+function addRecipientsEmail(e) {
+  const userFromList = e.target;
+
+  const userEl = document.createElement("li");
+  userEl.classList.add("recipient-item");
+  userEl.textContent = userFromList.textContent;
+
+  userFromList.classList.add("choosed");
+  userFromList.removeEventListener("click", addRecipientsEmail);
+
+  recipientsBodyEl.append(userEl);
 }
 
 // Receive an email
@@ -64,11 +82,25 @@ function handleEmail(e) {
     const userEmail = user.email.toLowerCase();
     const markPosition = userEmail.indexOf("@");
 
+    const recipientUsers = document.querySelectorAll(".recipient-item");
+    const recipients = Array.from(recipientUsers).map(
+      (recipient) => recipient.innerText
+    );
+    const recipientChoosedEmails = users
+      .filter((user) => recipients.includes(user.email))
+      .map((user) => user.email);
+
     if (userEmail.slice(0, markPosition).includes(email)) {
       const userItem = document.createElement("li");
       userItem.textContent = userEmail;
 
+      if (recipientChoosedEmails.includes(userEmail)) {
+        userItem.classList.add("choosed");
+      }
+
       userListEl.appendChild(userItem);
+
+      userItem.addEventListener("click", addRecipientsEmail);
     }
   });
 }
